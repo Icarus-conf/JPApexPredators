a6e5b977dd09ca91099e717cb60e3fab9ea9938e
Icarus-conf <brain.m.config@gmail.com>
2025-01-28 12:30:48 +0200

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Muhammad Fathy on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
