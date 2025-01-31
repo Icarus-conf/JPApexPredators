a65c993282d151e86f0a733d28f549562a5a39d7
Icarus-conf <brain.m.config@gmail.com>
2025-01-31 16:56:57 +0200

    last updates

---
+++ JPApexPredators/PredatorDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorDetail.swift
+//  JPApexPredators
+//
+//  Created by Muhammad Fathy on 30/01/2025.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorDetail()
+}
