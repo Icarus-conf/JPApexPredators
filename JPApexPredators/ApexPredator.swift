804d074a95b8ae078bcdff4424732f5b10995503
Icarus-conf <brain.m.config@gmail.com>
2025-01-31 17:05:13 +0200

    last update

---
+++ JPApexPredators/ApexPredator.swift
@@ -0,0 +1,70 @@
+//
+//  ApexPredator.swift
+//  JPApexPredators
+//
+//  Created by Muhammad Fathy on 28/01/2025.
+//
+import SwiftUI
+import MapKit
+
+struct ApexPredator: Decodable, Identifiable {
+    let id: Int
+    let name: String
+    let type: APType
+    let latitude: Double
+    let longitude: Double
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+    
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    struct MovieScene: Decodable, Identifiable {
+        let id: Int
+        let movie: String
+        let sceneDescription: String
+    }
+}
+
+enum APType: String, Decodable, CaseIterable, Identifiable {
+    var id: APType { self }
+    
+    case all
+    case land
+    case air
+    case sea
+    
+    var bgColor: Color {
+        switch self {
+        case .land:
+                .brown
+        case .air:
+                .teal
+        case .sea:
+                .blue
+        case .all:
+                .black
+        }
+    }
+    
+    var icon: String {
+        switch self {
+        case .land:
+            "square.stack.3d.up.fill"
+        case .air:
+            "leaf.fill"
+        case .sea:
+            "wind"
+        case .all:
+            "drop.fill"
+       
+        }
+    }
+    
+    }
